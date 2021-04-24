f9d78c3ef6efec705a926a6e4347212be864d52d
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-24 23:31:31 +0900

    html記述

--- Vue/mixApiCall/app.js
+++ Vue/mixApiCall/app.js
@@ -1,65 +1,26 @@
-// const 親component = {
-//   template: `
-//   <div>
-//     <h2>子供です</h2>
-//     <span v-bind:"props名"=""></span>
-//     <input v-on:click=""></input>
-//   </div>
-//   `
-// }
-
-// const 子component = {
-//   props: {
-//     /*親コンポーネント（UserList）がpropsを参照する時
-//     v-bind:user="ユーザーオブジェクト"
-//     と記述することになる
-//     え、ユーザーオブジェクトってなにかって？
-//     自分で作ってもいいし、他にあるやつ使えバカやろー
-//      */
-//     user: {
-//       type: Object
-//     }
-//   },
-//   template: `
-//     <div>
-//       <h1>propsで渡ってきました</h1>
-//       <h2>選択されたユーザー</h2>
-//       {{ user.name }}
-//     </div>
-//   `
-// }
-
-// const UserList = {
-//   components: {
-//     'list-title': ListTitle,
-//   },
-//   data: {
-//     users: [
-//       { id: 1, name: 'ユーザー１' },
-//       { id: 2, name: 'ユーザー2' },
-//       { id: 3, name: 'ユーザー3' },
-//       { id: 4, name: 'ユーザー4' },
-//       { id: 5, name: 'ユーザー5' }
-//     ]
-//   },
-//   template: `
-//     <div>
-//       <list-title></list-title>
-//       <ul>
-//         <li v-for="user in users" :key="user.id">
-//           {{ user.name }}
-//         </li>
-//       </ul>
-//      </div>
-//   `
-// }
-
-// const vm = new Vue({
-//   el: '#app',
-//   data: {
-
-//   },
-//   components: {
-//     'user-list': UserList
-//   }
-// })
+let vm = new Vue({
+  el: "#app",
+  data: {
+    title: "ユーザ一覧",
+    users: [],
+  },
+  created() {
+    this.users = [
+      {
+        id: 1,
+        name: "John Blue",
+        address: "unko@tinko.com",
+      },
+      {
+        id: 2,
+        name: "John Red",
+        address: "BigUnko@tinko.com",
+      },
+      {
+        id: 3,
+        name: "John Green",
+        address: "LargeUnko@tinko.com",
+      },
+    ];
+  },
+});
