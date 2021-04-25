26ade46890608c840bd4b711f18bff7295ac8843
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-25 12:31:17 +0900

    async await　パターン1

---
+++ JavaScript-async-await/app.js
@@ -0,0 +1,47 @@
+import fetch from "node-fetch";
+
+//これはasync/await
+const getGitUsername = async () => {
+  const message = "Gitのユーザー名は";
+  const url = "https://api.github.com/users/deatiger";
+
+  // GitHub APIをFetchメソッドで実行
+  const json = await fetch(url)
+    // .thenちぇーんがなくなる
+    .then((res) => {
+      console.log("success");
+      return res.json();
+    })
+    .catch((error) => {
+      console.error("これは非同期処理失敗時のメッセージです。", error);
+      return null;
+    });
+
+  const username = json.login;
+  console.log(username);
+  console.log(message + username);
+};
+
+getGitUsername();
+
+//これはfetch
+// 非同期処理をおこなう関数を宣言
+// const getGitUsername = () => {
+//   const url = "https://api.github.com/users/deatiger";
+
+//   GitHub APIをFetchメソッドで実行
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((json) => {
+//       console.log("これは非同期処理成功時のメッセージです");
+//       return json.login;
+//     })
+//     .catch((error) => {
+//       console.error("これは非同期処理失敗時のメッセージです。", error);
+//       return null;
+//     });
+// };
+
+// const message = "GitのユーザーIDは";
+// const username = getGitUsername();
+// console.log(message + username);
