ce3cd16aac6d0e8ac2a9cff939abfc2c5cf8f641
友造-2525 <bathbook1990tomozou@gmail.com>
2021-05-08 16:06:29 +0900

    Python*Docker

--- JavaScript-FizzBuzz/app.js
+++ JavaScript-FizzBuzz/app.js
@@ -1,11 +1,24 @@
-// const date = new Date()
-// const options = { era: 'long' };
-// console.log(new Intl.DateTimeFormat('ja-JP-u-ca-japanese', options).format(date));
-// console.log(date);
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+//   else if (i % 3 === 0) console.log("Fizz");
+//   else if (i % 5 === 0) console.log("Buzz");
+//   else console.log(i);
+// }
 
-for (let i = 0; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
-  else if (i % 3 === 0) console.log("Fizz");
-  else if (i % 5 === 0) console.log("Buzz");
-  else console.log(i);
+const date = new Date();
+const options = { era: "long" };
+console.log(
+  new Intl.DateTimeFormat("ja-JP-u-ca-japanese", options).format(date)
+);
+console.log(date);
+
+function drawFizzBuzz() {
+  for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+    else if (i % 3 === 0) console.log("Fizz");
+    else if (i % 5 === 0) console.log("Buzz");
+    else console.log(i);
+  }
 }
+
+drawFizzBuzz();
