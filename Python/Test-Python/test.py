deca9ccca98a04d18d529f80f2b59626bad446e3
友造-2525 <bathbook1990tomozou@gmail.com>
2021-05-17 08:19:47 +0900

    Python 基本構文

---
+++ Python/Test-Python/test.py
@@ -0,0 +1,73 @@
+#Unkomment
+
+print("Hello World!")
+
+print("unko")
+
+print("BigUnko!!")
+
+#演算
+print(1+1)
+print(1-1)
+print(8*9)
+print(10/2)
+print(5%3)
+
+#変数
+unko = "l_size"
+unko_length = 11
+unko_times = 132.5
+
+# print(type(unko))
+# print(type(unko_length))
+# print(type(unko_times))
+
+# boolean
+# unko_shitai = False
+
+# print(type(unko_shitai))
+
+# 条件演算子 < > == != | if elif else
+# if unko == "l_size":
+#   print("でっかちゃんだよ")
+
+# if unko == "s_size":
+#   print("でっかちゃんだよ")
+
+# if unko != "s_size":
+#   print("違うよ")
+
+# if unko_length < 300:
+#   print("すくない")
+
+# if unko_length < 10:
+#   print("if")
+# elif unko_length == 0:
+#   print("ねぇ")
+# else:
+#   print("ちょっちゅね")
+
+# 関数
+# def 関数名():
+#   条件
+#   条件式
+#     処理
+def unko_funbaru():
+  unko_status = 5
+
+  if (unko_status < 10):
+    return "まだだいじょぶ"
+  else:
+    return "やばい"
+
+print(unko_funbaru())
+
+def unko_funbaru(i):
+  unko_status = i
+
+  if (unko_status < 10):
+    return "まだだいじょぶ"
+  else:
+    return "やばい"
+
+print(unko_funbaru(5))
