2e6ee40d5e8dde825117af920bf15be49ec35241
友造-2525 <bathbook1990tomozou@gmail.com>
2021-05-18 00:16:45 +0900

    with openまで

--- Python/Test-Python/test.py
+++ Python/Test-Python/test.py
@@ -48,20 +48,24 @@ unko_times = 132.5
 #   print("ちょっちゅね")
 
 # 関数
+# 構文
 # def 関数名():
 #   条件
 #   条件式
 #     処理
-def unko_funbaru():
-  unko_status = 5
+# 
+#     引数なし
+# def unko_funbaru():
+#   unko_status = 5
 
-  if (unko_status < 10):
-    return "まだだいじょぶ"
-  else:
-    return "やばい"
+#   if (unko_status < 10):
+#     return "まだだいじょぶ"
+#   else:
+#     return "やばい"
 
-print(unko_funbaru())
+# print(unko_funbaru())
 
+# 引数あり
 def unko_funbaru(i):
   unko_status = i
 
@@ -70,4 +74,45 @@ def unko_funbaru(i):
   else:
     return "やばい"
 
-print(unko_funbaru(5))
+# print(unko_funbaru(5))
+
+# list
+unko_list = ["unko","tinko","anko"]
+# print(unko_list[1])
+
+# for文 構文
+# for x in xrange(1,10):
+#   pass
+
+# 基本的な使い方
+# for unko_list in range(5):
+#   print(unko_funbaru(5))
+
+# # カウント変数を見る
+# for x in range(10):
+#   print(x) 
+
+# # 応用
+# for index in range(11):
+#   print(unko_funbaru(index))
+
+# # 応用_変数に代入 listと組み合わせる
+# for item in unko_list:
+#   print(item)
+
+
+# with
+# open() 開いたファイルに追記や解析を施すことができる
+# 大体セットで用いられる
+# open関数・・・第一引数（読ませたいファイル）第二引数（モード）
+
+with open("./unko.txt", "r") as file:
+  print(file)
+  print(file.name)
+  print(file.mode)
+  print(file.read())
+# 処理の流れ
+# ①with open でファイルを指定
+# ②モードでr:読み取り w：書取り
+# ③as 変数名　で変数に格納
+# ④print で出力
