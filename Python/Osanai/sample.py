bc42b50cf3e8892eedbf59ce253d9fded1871e41
友造-2525 <bathbook1990tomozou@gmail.com>
2021-05-22 14:36:39 +0900

    Python 基本のデータ型

---
+++ Python/Osanai/sample.py
@@ -0,0 +1,10 @@
+print("Unko!!")
+number = 1
+number2 = 1.0
+greeting = "こんにちは"
+is_ok = True
+
+print(number, type(number))
+print(number2, type(number2))
+print(greeting, type(greeting))
+print(is_ok, type(is_ok))
