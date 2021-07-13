86de87c08b4946432d108bc178571329c4d8f5f6
友造-2525 <bathbook1990tomozou@gmail.com>
2021-07-13 16:49:32 +0900

    django app-helloworld

---
+++ Python/Practice/tinder.py
@@ -0,0 +1,13 @@
+setInterval(
+    function () {
+        const elem = document.getElementsByClassName("recsGamepad__button");
+        elem[3].click();
+    }, 1000)
+
+1アロー関数で定義
+2DOM操作したものを変数に
+3バリデーションもかけよう
+
+const pushLikeButton =
+
+setInterval(pushLikeButton,1000)
